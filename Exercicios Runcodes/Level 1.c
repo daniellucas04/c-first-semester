6204cc050f59a8756613018dd8eb9ca91b8eab03
unknown <daniellsilva@outlook.com>
2023-04-11 21:44:08 -0300

    Exercicio level 1 runcodes

---
+++ Exercicios Runcodes/Level 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// Faça um algoritmo para ler um valor do usuário e imprimi-lo no vídeo
+int main(void){
+	// Declarar a variavel
+	int x;
+	// Receber valor
+	scanf("%d", &x);
+	// Mostrar valor na tela
+	printf("%d", x);
+	return 0;
+}
+
