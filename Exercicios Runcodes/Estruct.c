0abe331c0bb48eea61831fc77883fbe935ab3323
unknown <daniellsilva@outlook.com>
2023-06-20 21:58:27 -0300

    fix: ideia of pointer

--- Exercicios Runcodes/Estruct.c
+++ Exercicios Runcodes/Estruct.c
@@ -13,7 +13,12 @@ int main(){
 	p=&x; //agora p aponta para x
 
 	(*p).dia = 31; // mesma coisa que x.dia=31
+	x.mes=6;
+	(*p).ano=2023;
 	printf("\n*p = %d", (*p).dia);
+	printf("\n*p = %d", (*p).mes);
+	printf("\n*p = %d\n", p->ano);
+
 	p -> dia=22; // mesmo efeito que (*p).dia=22
 	printf("\np = %d", p -> dia);
 	printf("\nx = %d", x.dia);
--- Exercicios Runcodes/Ponteiros.c
+++ Exercicios Runcodes/Ponteiros.c
@@ -1,20 +1,28 @@
 #include<stdio.h>
 
-int insert(int *ptr){
+void insert(int *ptr){
 	int i;
 	
-	for(i=0; i<10; i++)
-		scanf("%d", ptr[i]);
+	for(i=0; i<10; i++){
+		printf("arr[%d]: ", i, *(ptr+i));
+		scanf("%i", ptr+i);	
+	}
 		
-	return *ptr;
+}
+
+void print(int *ptr){
+	int i;
+
+	for(i=0; i<10; i++)
+		printf("arr[%d] = %d\n", i, *(ptr+i));
 }
 
 int main(){
-	int arr[10], i;
+	int arr[10]={1,2,3,4,5,6,7,8,9,10}, i;
 	int *ptr;
-	
-	ptr=&arr[10];
-	arr=insert(*ptr);
-	
+
+	ptr=arr;
+	insert(ptr);
+	print(ptr);
 	return 0;
 }
